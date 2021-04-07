c9d95a0bd5e1705be477f6a07fe12b47c3dc9089
melitus <asmelitus@gmail.com>
2021-04-07 13:08:22 +0100

    fix: Returned error: VM Exception while processing transaction: revert

--- pages/campaigns/new.tsx
+++ pages/campaigns/new.tsx
@@ -11,29 +11,29 @@ const CampaignNew = () => {
     const [loading, setLoading] = useState(false)
 
     const handleChange = event =>{
-        console.log({event: event.target.value})
         setMinimumContribution(event.target.value)
     }
+
+    const createCampaign = async() =>{
+    const instance = await factory()
+            const web3 = await loadWeb3()
+            const accounts = await web3.eth.getAccounts()
+          const campaignCreate =  await instance.methods
+            .createCampaign(minimumContribution)
+            .send({
+                from: accounts[0],
+                gas: 3000000
+            })
+           return campaignCreate
+}
     const handleSubmit = async (event) => {
         event.preventDefault() // this keep the browser fromm attempting to submit the form
         setLoading(true)
         setErrorMessage('')
         try {
-        const instance = await factory()
-        const web3 = await loadWeb3()
-        const accounts = await web3.eth.getAccounts()
-        console.log({accounts})
-        console.log({submit: minimumContribution})
-        // const contribution = Number(minimumContribution)
-        //         console.log({contribution: contribution})
-        await instance.methods
-           .createCampaign(minimumContribution)
-           .send({
-               from: accounts[0]
-           })
+          await createCampaign()
         } catch (error) {
-            setErrorMessage(error.message)
-         console.trace(error)
+           setErrorMessage(error.message)
         }
          setLoading(false)
     }
